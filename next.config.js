dc739d1b10cdc3de1914657ff3ba029860d3a8e1
Josh <adesanyajoshua@ymail.com>
2022-11-30 13:40:01 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ["images.unsplash.com", "ipfs.io"],
+  },
+  reactStrictMode: false,
+  env: {
+    customKey: "my-value",
+  },
+};
---
+++ utils/connectContract.js
@@ -0,0 +1,26 @@
+import abiJSON from "../utils/Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+  const contractAddress = "0x30147052420e25aE9D28A54cb0D07945Db9Eb514";
+  const contractABI = abiJSON.abi;
+  let rsvpContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum.chainId === "0x13881") {
+      //checking for eth object in the window, see if they have wallet connected to Mumbai network
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      console.log("contractABI", contractABI);
+      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+    } else {
+      throw new Error("Please connect to the Polygon Mumbai network.");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return rsvpContract;
+}
+
+export default connectContract;
